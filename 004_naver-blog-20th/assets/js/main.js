abed1502a666503e964ea4e878cb46401bedaa53
flowerkong3 <90238684+flowerkong3@users.noreply.github.com>
2023-11-28 10:42:31 +0900

    클론 | 네이버 블로그 20주년 웹사이트

---
+++ 004_naver-blog-20th/assets/js/main.js
@@ -0,0 +1,10 @@
+// var swiper = new Swiper(".swiper-rolling", {
+// 	autoplay: {
+//       delay: 0, //add
+//       disableOnInteraction: false,
+//     },
+//     speed: 3000,
+//     loop: true,
+//     loopAdditionalSlides: 1,
+//     slidesPerView: 5,
+//  });
